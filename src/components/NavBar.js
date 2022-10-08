048f8c5114cdad6f25018f63b6064a12410bfe67
Marqui-13 <98498681+Marqui-13@users.noreply.github.com>
2022-10-08 01:43:51 -0700

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
         <Link to='/'>
             <div className='navbar'>
                 <FaCoins className='icon' />
-                <h1> Coin <span className='gold'>Search</span></h1>
+                <h1> Coin <span className='gold'>List</span></h1>
             </div>
         </Link>
     )
