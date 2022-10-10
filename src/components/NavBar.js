8e11292dbf3fe0eff16a66118fdcaefcfac1e92a
Marqui <98498681+Marqui-13@users.noreply.github.com>
2022-10-10 03:32:41 -0700

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import {FaCoins, FaHouse} from 'react-icons/fa'
+import {FaCoins, BsHouseFill} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
 import './NavBar.css'
+
 
 const Navbar = () => {
     return (
         <Link to='/'>
             <div className='navbar'>
-                <FaHouse className='houseBtn' />
+                <BsHouseFill className='houseBtn' />
                 <FaCoins className='icon' />
                 <h1> Coin <span className='gold'>List</span></h1>
             </div>
